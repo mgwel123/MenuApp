1ace78e5f7e7bab0f8cbec950918c3c36240a0fa
Michael Welker <mikegw224@gmail.com>
2022-03-02 13:32:38 -0700

    new and replaces methods

--- ObjectOriented2.js
+++ ObjectOriented2.js
@@ -9,26 +9,21 @@ class Groceries {
 }
 
 class Cart {
-    constructor(name){
-        this.name = "My Cart";
+    constructor(){
         this.groceries = [];
     }
     addGroceries(groceries){
         if (groceries instanceof Groceries){
             this.groceries.push(groceries);
         }else {
-            throw new Error (`You may only add groceries on the list. ${groceries} is not listed.`)
+            throw new Error (`You may only add an instance of groceries. ${groceries} is not listed.`)
         }
-    }
-    listContents(){
-        return this.groceries.join(' * ');
     }
 }
 
 class Menu {
     constructor() {
         this.groceries = [];
-        this.cart = [];
         this.selectedGroceries = null;
     }
     start(){
@@ -36,18 +31,15 @@ class Menu {
         while (selection != 0) {
             switch (selection) {
                 case '1':
-                    this.addToList();
-                    break;
-                case '2':
                     this.addToCart();
                     break;
-                case '3':
+                case '2':
                     this.viewCart();
                     break;
-                case '4':
+                case '3':
                     this.removeFromCart();
                     break;
-                case '5':
+                case '4':
                     this.purchaseItems();
                     break;
                 default:
@@ -61,31 +53,53 @@ class Menu {
     showMainMenuOptions(){
         return prompt (`
         0) Exit
-        1) Add an item to Grocery List
-        2) Add items to Cart
-        3) View items in Cart
-        4) Remove an item from Cart
-        5) Purchase items in Cart
+        1) Add items to Cart
+        2) View items in Cart
+        3) Remove an item from Cart
+        4) Purchase items in Cart
         `);
     }
+    showGroceriesMenuOptions(groceryList){
+        return prompt (`
+        0) Back
+        1) Calculate total expenses
+        2) Remove an item
+
+        *************
+        ${groceryList}
+        `);
+    }
+
     viewCart() {
         let cartContents = '';
-        for (let i = 0; i < this.cart.length; i++){
-            cartContents += i + ') ' + this.cart[i] + '\n'
+        for (let i = 0; i < this.groceries.length; i++){
+            cartContents += i + ') ' + this.groceries[i].name + ' - ' + this.groceries[i].price + '\n';
         }
-        alert (cartContents);
+        let selection = this.showGroceriesMenuOptions(cartContents);
+        switch (selection) {
+            case '1':
+                this.totalExpenses();
+                break;
+            case '2':
+                this.removeFromCart();
+        }
     }
-    addToList(){
+
+    addToCart(){
         let name = prompt ("Please add an item to your list:");
         let price = prompt ("How much does it cost?");
         this.groceries.push(new Groceries(name, price));
     }
-    addToCart(){
-        let index = prompt (`List the index of the item you wish to add to your cart:
-        ---------------------
-        ${this.groceries}`);
-        if (index > -1 && index < this.groceries.length) {
-            this.selectedGroceries = this.groceries[index];
+    
+    removeFromCart(){
+
+    }
+
+    totalExpenses(){
+        let totalCost = 0;
+        for (let i = 0; i < this.groceries.length; i++){
+            totalCost += this.groceries[i].price;
+            return prompt (`Your total so far is: ${totalCost}`);
         }
     }
 }
